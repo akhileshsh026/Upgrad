8476149a997070dc5bb4cb0805d4f8f0f9a0fb86
Akhilesh Kumar Sahu <akhileshsh026@hotmail.com>
2019-03-26 15:36:02 +0530

    This the fabonaci series where we will have the 15th term as a final result

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-var x = prompt("Please Enter the number");
-num = parseInt(x);
-var sum= 0;
+// 1 1 2 3 5 8 13 21 34
 
-while(num>0)
+var a=1; 
+var b=1; 
+var f=a+b; 
+console.log(a+"\n"+b+"\n"+f);
+
+for(var i=4;i<15;i++)
 {
-   sum += (num%10);
-   num = Math.floor(num/10);
+    if(i%2==0)
+    {
+      a=f;
+      f=f+b;
+      console.log(f);
+    } else
+    {
+      b=f;
+      f = f+a;
+      console.log(f);
+    }
 }
-console.log(sum);
