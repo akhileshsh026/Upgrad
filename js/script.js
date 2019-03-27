c0086433bdc74955c8cb41448f7084d052e7ace6
Akhilesh Kumar Sahu <akhileshsh026@hotmail.com>
2019-03-27 10:44:57 +0530

    Testing array as a specifed string output

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
-// 1 1 2 3 5 8 13 21 34
+var shoppingList = ['Milk',2,'Bread',3,'Potato',12];
+var temp;
 
-var a=1; 
-var b=1; 
-var f=a+b; 
-console.log(a+"\n"+b+"\n"+f);
-
-for(var i=4;i<15;i++)
+for(var i=0;i<shoppingList.length;i++)
 {
-    if(i%2==0)
+       
+    if(i%2===0)
     {
-      a=f;
-      f=f+b;
-      console.log(f);
+        temp = (shoppingList[i] + "-");
+
     } else
     {
-      b=f;
-      f = f+a;
-      console.log(f);
+        temp += shoppingList[i];
+        console.log(temp);
+        temp=null;
     }
+    
 }
