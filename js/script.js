aeb27415c880f4e9bc3615b386b8f43cb67426dc
Akhilesh Kumar <akhileshsh026@hotmail.com>
2019-04-08 22:41:32 +0530

    push function in Nesting

--- js/script.js
+++ js/script.js
@@ -1,75 +1,46 @@
-// Confussing This Keywards example
 
-/* example 1
-
-var operation = {
-    a: 1,
-    b: 2,
-   
-    add: function() {
-        this.a + this.b;
-        console.log("a = " + this.a + ", b = " + this.b);
-        var print = function() {
-            console.log("a = " + this.a + ", b = " + this.b);
-        }
- 
-        print();
-    }
- }
- 
- operation.add();
-
- */
-
- /* Example 2 this keyward context 
-
-var person = {
-    firstName: "Sakshi",
-    get: function () {
-        console.log("Outer: " + this.firstName);
-        var print = function () {
-            console.log("Inner: " + this.firstName);
-        };
-        print();
-    }
- };
-
- person.get();
-
- */ 
-
-
-/* Example 4
-
-var title = "A Passage to India";
-var author = "E.M. Forster";
-
-var novel = {
-   title: "Pride and Prejudice",
-   author: "Jane Austen",
-   print: function() {
-       console.log(this.title);
-       console.log(this.author);
-   }
-}
-
-novel.print();
-
+/*
+For example, you need to add {"variable": "var3", "matchType": "Regex", "value": ".*"} to 
+the variables property from request Object...
 */
 
-var novel1 = {
-    title: "Pride and Prejudice",
-    author: "Jane Austen",
-    get: function() {
-        console.log("Title = " + this.title + ", Author = " + this.author);
-    }
- }
- 
- novel2 = {
-    title: "A Passage to India",
-    author: "E.M. Forster"
- }
- 
- novel2.get = novel1.get;
- novel2.get();
- novel1.get();
+var request = {
+    "name": "Name",
+    "id": 3,
+    "rules":[
+      {
+        "name": "Rule name",
+        "tags": [
+          {
+            "tagId": 1,
+            "variables":[
+              {
+                "variable": "var1",
+                "matchType": "Regex",
+                "value": ".*"
+              },
+              {
+                "variable": "var2",
+                "matchType": "Regex",
+                "value": ".*"
+              }
+            ],
+            "condition": false,
+          },
+          {
+            "tagId": 1,
+            "condition": false,
+          }
+        ],
+        "ruleSetId": 3,
+      }
+    ]
+  }
+
+  // You have to "navigate" properly in your object:
+  
+  request.rules[0].tags[0].variables.push({
+    "variable":"var3",
+    "matchType": "Regex",
+    "value": ".*"
+  })
