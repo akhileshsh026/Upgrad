3a356cb7479d95623db01da3cc405355d7c74698
Akhilesh Kumar <akhileshsh026@hotmail.com>
2019-04-08 09:10:23 +0530

    example 5 Correct version

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 function createEmployeeID() {
     var prefix = "employee";
-    return generateID();
- }
- 
- var generateID = function() {
-    for (var i = 0; i < 5; i++) {
-        console.log(prefix + i);
+    var generateID = function() {
+        for (var i = 0; i < 5; i++) {
+            console.log(prefix + i);
+        }
     }
+    return generateID;
  }
  
  var print = createEmployeeID();
+ console.log(print());
 
- /*
- 
- The anonymous function contained in the variable generateID is not written inside the function createEmployeeID(). It is just called inside the function createEmployeeID(). The scope of both these functions are different. This is why, the variable prefix (declared inside the function named createEmployeeID) is not inside the scope of the function generateID. Thus, this option is the correct choice.
+ /* 
 
- */
+This is an example of closures. The anonymoous function inside the variable generateID is defined inside the lexical scope of the function createEmployeeID(). 
+The inner function represented by generateID gets to access all the resources of the outer function named createEmployeeID() and thus, the variable prefix is accessible inside the inner function. Thus, when the inner function is returned from the outer function, it still maintains the value of the prefix variable. Thus, this code prints the prefix value - employee suffixed by the loop’s iteration variable i’s value, thereby printing employee0 to employee4. Therefore, this option is the correct choice
+
+*/
