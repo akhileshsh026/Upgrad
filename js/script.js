f92e0c13071593b10c4be768dac60cea1634c21b
Akhilesh Kumar <akhileshsh026@hotmail.com>
2019-04-08 09:33:41 +0530

    solution to the last question

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ var generateID = function() {
          var prefix = "uid-" , count=0; 
          return  function create () {
                         count+=1;
-                        console.log(prefix+count);
+                        return prefix+count;
                         }
 };
 
 var create = generateID();
 
-console.log(create(),create(),create());
+console.log(create());
+console.log(create());
+console.log(create());
