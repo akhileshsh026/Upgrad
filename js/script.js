2dee98a8f44c3f44ae61439880502a3c1b300a56
Akhilesh Kumar <akhileshsh026@hotmail.com>
2019-04-08 09:29:20 +0530

    problem that the function is returing 'undefined' after the result

--- js/script.js
+++ js/script.js
@@ -1,19 +1,11 @@
-function createEmployeeID() {
-    var prefix = "employee";
-    var generateID = function() {
-        for (var i = 0; i < 5; i++) {
-            console.log(prefix + i);
-        }
-    }
-    return generateID;
- }
- 
- var print = createEmployeeID();
- console.log(print());
+var generateID = function() {
+         var prefix = "uid-" , count=0; 
+         return  function create () {
+                        count+=1;
+                        console.log(prefix+count);
+                        }
+};
 
- /* 
+var create = generateID();
 
-This is an example of closures. The anonymoous function inside the variable generateID is defined inside the lexical scope of the function createEmployeeID(). 
-The inner function represented by generateID gets to access all the resources of the outer function named createEmployeeID() and thus, the variable prefix is accessible inside the inner function. Thus, when the inner function is returned from the outer function, it still maintains the value of the prefix variable. Thus, this code prints the prefix value - employee suffixed by the loop’s iteration variable i’s value, thereby printing employee0 to employee4. Therefore, this option is the correct choice
-
-*/
+console.log(create(),create(),create());
