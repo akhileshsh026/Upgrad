5045a130ef4d6a6ce09597727cc016ac8eaed144
Akhilesh Kumar <akhileshsh026@hotmail.com>
2019-04-09 10:41:26 +0530

    apply method which takes arguments as a array elements

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var person = {
         console.log("outer : My name is "+ this.firestName + this.lastName);
         var print = function(role,role2) {
             console.log("Inner : My name is "+ this.firestName + this.lastName + " My role 1 and role 2 are :" + role + ","+ role2);
-        }.call(this,"dev","devops")
+        }.apply(this,["dev","devops"])
     }
 }
 
