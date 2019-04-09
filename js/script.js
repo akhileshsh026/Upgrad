c90ca2218b670733f4d5e91040ec87b9c5ad7ad1
Akhilesh Kumar <akhileshsh026@hotmail.com>
2019-04-09 09:31:56 +0530

    Example of Bind method in , aslo there are sevral other way this is 1st.

--- js/script.js
+++ js/script.js
@@ -1,46 +1,13 @@
+var person = {
+    firestName : "Akhilesh kumar",
+    lastName : "Sahu",
+    get : function() {
+        console.log("outer : My name is "+ this.firestName + this.lastName);
+        var print = function() {
+            console.log("Inner : My name is "+ this.firestName + this.lastName);
+        }.bind(this) 
+        print();
+    }
+}
 
-/*
-For example, you need to add {"variable": "var3", "matchType": "Regex", "value": ".*"} to 
-the variables property from request Object...
-*/
-
-var request = {
-    "name": "Name",
-    "id": 3,
-    "rules":[
-      {
-        "name": "Rule name",
-        "tags": [
-          {
-            "tagId": 1,
-            "variables":[
-              {
-                "variable": "var1",
-                "matchType": "Regex",
-                "value": ".*"
-              },
-              {
-                "variable": "var2",
-                "matchType": "Regex",
-                "value": ".*"
-              }
-            ],
-            "condition": false,
-          },
-          {
-            "tagId": 1,
-            "condition": false,
-          }
-        ],
-        "ruleSetId": 3,
-      }
-    ]
-  }
-
-  // You have to "navigate" properly in your object:
-  
-  request.rules[0].tags[0].variables.push({
-    "variable":"var3",
-    "matchType": "Regex",
-    "value": ".*"
-  })
+person.get();
