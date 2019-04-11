236da3c4504de8fe718a6f8da70b1e403e808866
Akhilesh Kumar <akhileshsh026@hotmail.com>
2019-04-11 16:40:11 +0530

    Examples of bind ,call, apply methods

--- js/script.js
+++ js/script.js
@@ -1,28 +1,52 @@
-var laptop = {
-    company: "Apple",
-    model: "Macbook Air",
-    os: "macOS",
-    set: function (company, model, os) {
-        var setCompany = function (company) {
-            this.company = company;
-        }.bind(this, company);
- 
-        var setModel = function (model) {
-            this.model = model;
-        }.bind(this);
- 
-        var setOS = function (os) {
-            this.os = os;
-        }
- 
-        setCompany();
-        setModel();
-        setOS();
-    },
-    get: function() {
-        console.log(this.company, this.model, this.os);
+var team = {
+    participantOne: "Srishti",
+    participantTwo: "Prateek",
+    print: function () {
+        /* TODO 1: 
+        A. Define a function with identifier greetParticipants
+        B. Pass the scope of the team object to greetParticipants function using bind() method
+        C. The function greetParticipants must print the message to welcome both participants defined in the team object on the console, as shown in Problem Statement
+        */
+       var greetParticipants = function() {
+           console.log("Welcome " + this.participantOne +" " + this.participantTwo);
+       }.bind(this)
+     greetParticipants();
     }
- };
- 
- laptop.set("Dell", "Inspiron", "Windows");
- laptop.get();
+}
+
+team.print();
+
+
+var team = {
+    participantOne: "Srishti",
+    participantTwo: "Prateek",
+    print: function () {
+        /* TODO 2: 
+        A. Define a function with identifier greetParticipants
+        B. Pass the scope of the team object to greetParticipants function using call() method
+        C. The function greetParticipants must print the message to welcome both participants defined in the team object on the console, as shown in Problem Statement
+        */
+       var greetParticipants = function() {
+        console.log("Welcome " + this.participantOne +" " + this.participantTwo);
+       }.call(this)
+    }
+}
+
+team.print();
+
+
+var team = {
+    participants: ["Srishti", "Prateek"],
+    print: function () {
+        /* TODO 3: 
+        A. Define a function with identifier greetParticipants
+        B. Pass the scope of the team object to greetParticipants function using apply() method
+        C. The function greetParticipants must print the message to welcome both participants defined in the team object on the console, as shown in Problem Statement
+        */
+       var greetParticipants = function() {
+        console.log("Welcome " + this.participantOne +" " + this.participantTwo);
+       }.apply(this,team.participants);
+    }
+};
+
+team.print();
