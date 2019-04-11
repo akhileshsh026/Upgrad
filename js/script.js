534b6024a0a2d278d6f597ad740f5434070454f4
Akhilesh Kumar <akhileshsh026@hotmail.com>
2019-04-11 17:23:33 +0530

    Example 4,5

--- js/script.js
+++ js/script.js
@@ -1,12 +1,46 @@
-var person = {
-    name: "Mayank Kumar",
-    gender: "male"
- }
+/*
+var title = "A Passage to India";
+var author = "E.M. Forster";
+
+var novel = {
+   title: "Pride and Prejudice",
+   author: "Jane Austen",
+   get: function() {
+       console.log("Title = " + this.title + ", Author = " + this.author);
+
+       var print = function() {
+           console.log("Title = " + this.title + ", Author = " + this.author);
+       }
+       print();
+   }
+}
+
+novel.get();
+
+*/
+
+
+var laptop = {
+    company: "Apple",
+    model: "Macbook Air",
+    os: "macOS",
+    set: function (company, model, os) {
+        var setCompany = function (company) {
+            this.company = company;
+        }.bind(this, company);
  
- var employee = {
-    id: 1001
- }
+        var setModel = function (model) {
+            this.model = model;
+        }.call(this, model);
  
- employee.__proto__ = person;
- console.log(person.__proto__);
- console.log(employee.__proto__);
+        var setOS = function (os) {
+            this.os = os;
+        }.apply(this, os);
+    },
+    get: function () {
+        console.log("Company = " + this.company + "\nModel = " + this.model + "\nOS = " + this.os);
+    }
+ };
+ 
+ laptop.set("Dell", "Inspiron", "Windows");
+ laptop.get();
