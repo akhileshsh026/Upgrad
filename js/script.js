aa8fabd9dcbdafdf3689aaa05020e266a3225808
Akhilesh Kumar <akhileshsh026@hotmail.com>
2019-04-19 14:15:01 +0530

    MAP() Reduce() Filter() Methods

--- js/script.js
+++ js/script.js
@@ -1,39 +1,38 @@
-//Map
+/*
 
-const words = ["Kitkat", "Lollipop", "Marshmallow", "Noughat", "Oreo"];
-var temp="";
-words.map(function(item) {
-  temp += (item+",");
+var input = [23,45,678,90,33,21,780,670,498,7493,58940,6565,900,370,1299,1300];
+var output = [];
+
+// getting multiples of 10
+input.filter(function(item){
+        if(item%10==0)
+           {
+             output.push(item);
+             // return output;
+           }
+});
+console.log(output);
+*/
+
+/*
+
+var input = [189.45, 12.89, 7.43, 167.98, 767.78, 23.89, 990.67, 289.7, 44.56, 97.65, 2345.87, 665.43, 7221.89, 58843.21];
+var output = [];
+//console.log(Math.floor(input[0]) - input[0]);
+
+input.map(function(item){
+    output.push(item*100); 
 })
-console.log(temp.substring(0,40));
 
-//Filter
+console.log(output);
 
-words.filter(function(item) {
-  if(item == "Oreo")
-  console.log(item + "Item Found");
+*/
+
+var input = [234, 567, 273, 456, 598, 635, 644, 718, 750, 753, 801];
+var output =[];
+
+output = input.reduce(function(x,y){
+  return x+y;
 })
 
-const a = [10, 20, 30];
-a.filter(function(item) {
-  if(item>=15)
-  console.log(item);
-})
-
-
-function sums(a) {
-   let sum=0;
-   for (let i = 0; i < a.length; i++) {
-      sum +=a[i]; 
-   }
-   console.log(sum);
-}
-
-sums(a);
-
-//Reduce
-const b = [10, 20, 30,40,50,60,70,80];
-b.reduce(function(x,y){
-  console.log(" X :"+ x," Y :"+ y);
-  return y/x; // any operation u can do here x+y x*y 
-})
+console.log(output);
