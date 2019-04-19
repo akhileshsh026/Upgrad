2963fe37da6238fca107bab54a630fd74717b2d9
Akhilesh Kumar <akhileshsh026@hotmail.com>
2019-04-19 18:01:31 +0530

    Problem Statement

--- js/script.js
+++ js/script.js
@@ -1,38 +1,33 @@
-/*
+/* Problem Statement 
 
-var input = [23,45,678,90,33,21,780,670,498,7493,58940,6565,900,370,1299,1300];
-var output = [];
-
-// getting multiples of 10
-input.filter(function(item){
-        if(item%10==0)
-           {
-             output.push(item);
-             // return output;
-           }
-});
-console.log(output);
-*/
-
-/*
-
-var input = [189.45, 12.89, 7.43, 167.98, 767.78, 23.89, 990.67, 289.7, 44.56, 97.65, 2345.87, 665.43, 7221.89, 58843.21];
-var output = [];
-//console.log(Math.floor(input[0]) - input[0]);
-
-input.map(function(item){
-    output.push(item*100); 
-})
-
-console.log(output);
+Description
+There are two classes – class A and class B. Class A consists of 4 students – Alice, Bob, Catherine, and Dan. Class B consists of 4 students – George, Jane, Katy, Luis. Note that the names of the students in both the classes are stored in individual arrays in the alphabetical order. On a particular day, both the classes are combined and two new students are admitted to the class – Emily and Frank.
+ 
+You are required to merge both the arrays representing the two classes and merge the two new students into a single array, which maintains the alphabetical order of storing students’ names. At the end, print this merged array on to the console.
+ 
+Thus, the output will be:
+(10) ["Alice", "Bob", "Catherine", "Dan", "Emily", "Frank", "George", "Jane", "Katy", "Luis"]
 
 */
 
-var input = [234, 567, 273, 456, 598, 635, 644, 718, 750, 753, 801];
-var output =[];
+const mergeStudents = (classA, classB) => {
+  /* TODO 1: Create a variable with identifier mergedClass
+  Variable mergedClass must contain the classA's students, then two students "Emily" and "Frank", and then class B's students 
+  */
+  let mergedClass = []; 
+  mergedClass = [...classA,...classB];
+  mergedClass.push("Emily","Frank");
+  mergedClass.sort();
+  
 
-output = input.reduce(function(x,y){
-  return x+y;
-})
+    
+  // TODO 2: Print the variable mergedClass on the console
+  console.log("("+mergedClass.length+")"+JSON.stringify(mergedClass));
+  
+}
 
-console.log(output);
+// arrays containing names of students in two classes
+const classA = ["Alice", "Bob", "Catherine", "Dan"];
+const classB = ["George", "Jane", "Katy", "Luis"];
+
+mergeStudents(classA, classB);
