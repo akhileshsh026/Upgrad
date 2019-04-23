c3d649d5f456bc9a418c175d038617263e0da973
Akhileshsh028 DB wala <akhielshsh028@outlook.com>
2019-04-23 09:21:24 +0530

    Additional info from discussion fourm.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
-class Person {
-    constructor() {
-        console.log("I am a person.");
-    }
- }
- 
- class Employee extends Person {
-    constructor() {
-        console.log("I am an employee.");
-    }
- }
- 
- let p = new Person();
- let e = new Employee();
+console.log('1) Passing function as a context in bind method');
 
+let printDetails = function(fname, lname, role) {
+    console.log(fname + " " + lname + ". I am a " + role + ".");
+}
+var person3 = {
+    firstName: "John",
+    lastName: "Doe",
+    get: function () {
+        var print = function (fname, lname, role) {
+           this(fname, lname, role);
+        }.bind(printDetails, this.firstName, this.lastName, 'Tester');
+        print();
+    }
+ };
+
+ person3.get();
+
+
+ console.log('2) Passing the Object as context.');
+ var person2 = {
+    details: {
+        firstName: "Roshan",
+        lastName: "Menon"
+    },
+    get: function () {
+        var print = function (role) {
+            console.log(this.firstName + " " + this.lastName + ". I am a " + role + ".");
+        }.bind(this.details, "Developer");
+        print();
+    }
+ };
+ 
+ person2.get();
