7549d3b262b8f05780512478aa91e04b9df1d9ae
Akhileshsh028 DB wala <akhielshsh028@outlook.com>
2019-04-23 08:53:09 +0530

    IMpoertant POint in the question

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-/* 
-1 method
-
-import {sum,avg} from './util1.ja';
-
-2 nd method
-
-import {sum as add} from './util.ja'; 
-  
-3rd method  * as an aliase
-*/
-import * as fn from './util1.js';
-
-console.log(fn.sum(1,2,3,4));
-console.log(fn.avg(1,2,3,4));
+function login(loggedIn) {
+    message = "Please login!";
+    console.log(message);
+    if (loggedIn) {
+        name = "Srishti";
+        let name;
+        var message = "Welcome " + name + "!";
+        console.log(message);
+    }
+ }
+ 
+ login(true);
