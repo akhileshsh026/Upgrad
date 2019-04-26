5038dd92ada8939729838446104ccac5a1b4abbf
Akhileshsh028 DB wala <akhielshsh028@outlook.com>
2019-04-26 17:35:55 +0530

    the Genric functions oking schema.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
-const foo = (anotherFunction) => {
-  console.log("Inside foo");
-  anotherFunction();
+console.log("Genric function example");
+
+function add(x,y)
+{
+  return x+y;
 }
 
-const bar = () => {
-  console.log("Inside bar");
+function multiply(x,y)
+{
+   return x*y;
 }
 
-foo(bar);
+function Genric_functions(x,y,operate)
+{
+    return operate(x,y);
+}
+
+console.log("using sigle call stamente to use functions : add");
+console.log(Genric_functions(5,6,add));
+console.log("using sigle call stamente to use functions : Multiply");
+console.log(Genric_functions(5,6,multiply));
+
+
+function Genric_functions1(x,y,operate)
+{
+  x +=y;
+  y +=x;
+  return operate(x,y,operate);
+}
+
+console.log("using sigle call stamente to use functions Custom opertaions : add");
+console.log(Genric_functions1(5,6,add));
+console.log("using sigle call stamente to use functions Custom opertaions : Multiply");
+console.log(Genric_functions1(5,6,multiply));
+
