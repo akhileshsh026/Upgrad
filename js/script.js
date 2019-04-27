eef4bac9f6c3712cb92dad777dbd2c2bb6986ee5
Akhileshsh028 DB wala <akhielshsh028@outlook.com>
2019-04-27 15:53:22 +0530

    Passing function as arguments.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,10 @@
-console.log("Genric function example");
-
-function add(x,y)
-{
-  return x+y;
+const foo = (anotherFunction) => {
+  console.log("Inside foo");
+  anotherFunction();
 }
 
-function multiply(x,y)
-{
-   return x*y;
+const bar = () => {
+  console.log("Inside bar");
 }
 
-function Genric_functions(x,y,operate)
-{
-    return operate(x,y);
-}
-
-console.log("using sigle call stamente to use functions : add");
-console.log(Genric_functions(5,6,add));
-console.log("using sigle call stamente to use functions : Multiply");
-console.log(Genric_functions(5,6,multiply));
-
-
-function Genric_functions1(x,y,operate)
-{
-  x +=y;
-  y +=x;
-  return operate(x,y,operate);
-}
-
-console.log("using sigle call stamente to use functions Custom opertaions : add");
-console.log(Genric_functions1(5,6,add));
-console.log("using sigle call stamente to use functions Custom opertaions : Multiply");
-console.log(Genric_functions1(5,6,multiply));
-
+foo(bar);
