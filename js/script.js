6463aa4a1598c51b82548e7ae62a31d647f136b8
Akhileshsh028 <akhileshsh028@outlook.com>
2019-04-29 16:25:42 +0530

    Taking the input from user in console in node js.

--- js/script.js
+++ js/script.js
@@ -1,44 +1,9 @@
-const sample = (username , password ,callback1) => {
-    let ans  = prompt("press 1 for 1 args , 2 for 2 args , 3 for 3 args.");
-     switch (ans) {
-         case "1":
-               let error = "Error single parameter.";
-               callback1(error);
-             break;
-         case "2":
-                callback1(null,username);
-             break;
-         case "3":
-               callback1(null,username,password);
-             break;
-     
-         default:
-               console.log("Don't time pass");
-             break;
-     }
-}
-
-const callback1 = (error,username,password) => {
-    if(error)
-    {
-        console.log(error);
-    }
-    if(username)
-    {
-        console.log(username);
-    }
-    if(password)
-    {
-        console.log(password);
-    }
-}
-
-
-window.onload = ()=>{
-  document.getElementById('demo').addEventListener('click',()=>
-  {
-      sample("Akhileshsh026","Akhilesh@123",callback1);
-  })
-};
-
-
+const readline = require('readline').createInterface({
+    input : process.stdin,
+    output : process.stdout
+ })
+ 
+ readline.question('What is your name', (name)=>{
+    console.log(`Hi! ${name}`);
+    readline.close();
+ })
