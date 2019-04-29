a36c0f73d87dc26b7c479f81d96936a16700ba12
Akhileshsh028 <akhileshsh028@outlook.com>
2019-04-29 16:21:06 +0530

    very important concept you can call a function who require max 3 args you can sully any no of args and use that function according to your use case

--- js/script.js
+++ js/script.js
@@ -1,29 +1,44 @@
-const fetchUserDetails = (username, password, callback) => {
-    database.authenticateUser(username, password, (error, userInfo) => {
-        if (error) {
-            callback(error);
-        } else {
-            database.getRoles(userInfo, (error, rolesInfo) => {
-                if (error) {
-                    callback(error);
-                } else {
-                    database.getPermissions(rolesInfo, (error, permissionsInfo) => {
-                        if (error) {
-                            callback(error);
-                        } else {
-                            callback(null, userInfo, rolesInfo, permissionsInfo);
-                        }
-                    });
-                }
-            });
-        }
-    });
+const sample = (username , password ,callback1) => {
+    let ans  = prompt("press 1 for 1 args , 2 for 2 args , 3 for 3 args.");
+     switch (ans) {
+         case "1":
+               let error = "Error single parameter.";
+               callback1(error);
+             break;
+         case "2":
+                callback1(null,username);
+             break;
+         case "3":
+               callback1(null,username,password);
+             break;
+     
+         default:
+               console.log("Don't time pass");
+             break;
+     }
 }
 
-const callback = (error, userInfo, rolesInfo, permissionsInfo) => {
-    // some code here
+const callback1 = (error,username,password) => {
+    if(error)
+    {
+        console.log(error);
+    }
+    if(username)
+    {
+        console.log(username);
+    }
+    if(password)
+    {
+        console.log(password);
+    }
 }
 
 
+window.onload = ()=>{
+  document.getElementById('demo').addEventListener('click',()=>
+  {
+      sample("Akhileshsh026","Akhilesh@123",callback1);
+  })
+};
 
 
