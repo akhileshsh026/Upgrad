e7f97bfd9f628a29f30d750ed3f97c88b911ddff
akhileshsh026 <akhileshsh026@hotmail.com>
2019-06-09 23:58:47 +0530

    exports another way

--- js/script.js
+++ js/script.js
@@ -4,5 +4,3 @@ let circle = require('./script1');
 
 console.log(circle.calculateArea(2));
 console.log(circle.calculateCircumfrence(3));
-console.log(calculateSum(2));
-console.log(calculateProduct(3));
--- js/script1.js
+++ js/script1.js
@@ -2,21 +2,8 @@ const pi = 3.14;
 
 
 // ANother way of exporting becuase the exports points out to the module.exports.
-const calculateArea = r => 2*pi*r;
+exports.calculateArea = r => 2*pi*r;
 
-const calculateCircumfrence = r => pi * r*r;
+exports.calculateCircumfrence = r => pi * r*r;
 
-// also another way 
 
-calculateSum  =  r => r+r;
-
-calculateProduct = r=> r*r;
-
-exports = {
-    calculateSum :calculateSum ,
-    calculateProduct : calculateProduct ,
-    calculateArea :calculateArea,
-    calculateCircumfrence : calculateCircumfrence
-};
-
-module.exports = exports;
