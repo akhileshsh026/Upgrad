5b68379040a47addc0ac9a4b571e06b7fb57206e
akhileshsh026 <akhileshsh026@hotmail.com>
2019-06-09 23:54:59 +0530

    Using Exports ehich points to moduel to export object.

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ let circle = require('./script1');
 
 
 
-console.log(circle);
+console.log(circle.calculateArea(2));
+console.log(circle.calculateCircumfrence(3));
+console.log(calculateSum(2));
+console.log(calculateProduct(3));
--- js/script1.js
+++ js/script1.js
@@ -1,5 +1,22 @@
 const pi = 3.14;
 
-module.exports.calculateArea = r => 2*pi*r;
 
-module.exports.calculateCircumfrence = r => pi * r*r;
+// ANother way of exporting becuase the exports points out to the module.exports.
+const calculateArea = r => 2*pi*r;
+
+const calculateCircumfrence = r => pi * r*r;
+
+// also another way 
+
+calculateSum  =  r => r+r;
+
+calculateProduct = r=> r*r;
+
+exports = {
+    calculateSum :calculateSum ,
+    calculateProduct : calculateProduct ,
+    calculateArea :calculateArea,
+    calculateCircumfrence : calculateCircumfrence
+};
+
+module.exports = exports;
