24701797d50d693f3bac5e22bf054affa075a770
akhileshsh026 <akhileshsh026@hotmail.com>
2019-06-10 20:27:14 +0530

    Fs operation in the app.js heaving the mode as w write.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log(require('module').builtinModules);
+const fs = require('fs');
+
+let data = "Hello node js i will cook you out in my brain.";
+let options = {
+    encoding : 'utf8',
+    flag : 'w'
+};
+
+fs.writeFile('hello.txt',data,options,(err) => {
+    if(err)
+    {
+        console.log('Error Occured');
+    } else
+    {
+        console.log('Operation Completed Sucessfully');
+    }
+})
