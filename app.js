2d0e4f4a8411a1cd93defd09fa9bbb2fd0e4c700
akhileshsh026 <akhileshsh026@hotmail.com>
2019-06-10 21:33:30 +0530

    Program to segreigate the array with the provided dilemeter.

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const fs = require('fs');
+// TODO 1: Complete the function as per the definition given below (Make sure that the name of the function is 'convertArrToStr'.)
 
-let data = "Those who understand binary or not .";
-let options = {
-    encoding : 'utf8',
-    flag : 'a'
-};
+/**
+ * Function to convert the given array to string
+ * @param {*} arr - input array which needs to be converted to string
+ * @param {*} delimiter - delimiter which separates elements of the array
+ * @returns {string} array converted to string
+ */
+function convertArrToStr(arr,delimiter) {
+    let result= "" ;   
+    arr.map( element => {
+           result += (element + delimiter) ; 
+       })
+       result = result.substring(0,(result.length-1));
+       return result;
+}
 
-fs.writeFileSync('hello.txt',data,options);
+
+// TODO 2: Export the function convertArrToStr defined above
+
+
+// WARNING: DO NOT EDIT THE CODE GIVEN BELOW (If you fail to do so, your solution will not pass the test cases!)
+console.log(convertArrToStr([1, 2, 3, 4, 5], ","));
+console.log(convertArrToStr(["Alpha", "Beta", "Gamma"], "-"));
