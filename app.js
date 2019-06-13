08c3c9389304dba62afba69e8e1ffbde1b146ff5
Allwin112 <allwincharles31@gmail.com>
2019-06-13 16:24:08 +0530

    1st step of genrating a block

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-// TODO 1: Complete the function as per the definition given below (Make sure that the name of the function is 'convertArrToStr'.)
+class block
+{
+       constructor(index,transaction,hash)
+       {
+           this.index = index;
+           this.transaction = transaction ;
+           //this.timestamp = new Date().getTime();
+           this.hash = hash ;
+       };
 
-/**
- * Function to convert the given array to string
- * @param {*} arr - input array which needs to be converted to string
- * @param {*} delimiter - delimiter which separates elements of the array
- * @returns {string} array converted to string
- */
-function convertArrToStr(arr,delimiter) {
-    let result= "" ;   
-    arr.map( element => {
-           result += (element + delimiter) ; 
-       })
-       result = result.substring(0,(result.length-1));
-       return result;
+       toString() {
+           return JSON.stringify(this);
+       }
 }
 
+let GenesisBlock = new block(0,[],"This is Hash.");
 
-// TODO 2: Export the function convertArrToStr defined above
-
-
-// WARNING: DO NOT EDIT THE CODE GIVEN BELOW (If you fail to do so, your solution will not pass the test cases!)
-console.log(convertArrToStr([1, 2, 3, 4, 5], ","));
-console.log(convertArrToStr(["Alpha", "Beta", "Gamma"], "-"));
+console.log(GenesisBlock.toString());
