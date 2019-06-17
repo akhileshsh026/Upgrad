bdbb7061c7f4a3ea814bf3ce4b939b4c380594aa
Akhileshsh028 <akhileshsh028@outlook.com>
2019-06-17 16:20:48 +0530

    Generating the Public and Private Key Pairs using the elliptc liberary.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const crypto = require('crypto');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
 class Block
 {
@@ -24,4 +26,11 @@ for (let index = 1; index < 10 ; index++) {
     
     blockChain.push(new Block(index,[], new Date().getTime(),blockChain[index-1].hash));
 }
-console.log(blockChain);
+
+//console.log(blockChain);
+let key = ec.genKeyPair();
+
+
+console.log(`Public Key points : [ ${key.getPublic().getX().toString() } , ${ key.getPublic().getY().toString()} ]`);
+console.log(`Pulic Key : 04${key.getPublic().getX().toString('hex') + key.getPublic().getY().toString("hex")}`);
+console.log(`Private Key : ${key.getPrivate('hex').toString()}`);
