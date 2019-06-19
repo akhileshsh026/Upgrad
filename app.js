d0a05401f7f15807a64d8271278136d5f640f999
Akhileshsh028 <akhileshsh028@outlook.com>
2019-06-19 11:15:07 +0530

    The dymaic Difficulty intoduced in the network where if the difficulty is increaed by reducing the difficulty value and vice versa to decrease the difficulty in the netwrok

--- app.js
+++ app.js
@@ -1,50 +1,112 @@
 const crypto = require('crypto');
 
-class Block {
- constructor(index, transactions, prevHash, nonce){
-   this.index = index;
-   this.transactions = transactions;
-   if (typeof timestamp === "undefined") {
-     this.timestamp = new Date().getTime();
-   } else {
-     this.timestamp = timestamp
-   }
+let genesisBlock = {
+  index: 0,
+  transactions: [],
+  difficulty: "000000000000000000000000000000000ffffffffffffffffffffffffff",
+  timestamp: 1536907693191,
+  nonce: 238199,
+  prevHash: 'fffffffffffffffffffffffff',
+  hash: '36a1517830a8a71a50cc619a4991ca5a1fef0289d82e8276e62a93263cb718f5',
+};
 
-   if (typeof nonce === "undefined") {
-     this.nonce = 0;
-   } else {
-     this.nonce = nonce;
-   }
-
-   this.hash = crypto.createHash('sha256').update(this.toString()).digest('hex');
-   this.prevHash=prevHash;
- };
-
- updateNonce() {
-   this.nonce += 1;
-   let updatedBlock = new Block(this.index, this.transactions, this.prevHash, this.nonce);
-   this.hash = updatedBlock.hash;
- }
-
- toString() {
-   return JSON.stringify(this);
- }
-}
-let genesisBlock = new Block(0, [], new Date().getTime(), "this_is_hash_for_block_0", "this_is_prev_hash");
-
+let difficultyStep = "ffffffffffffffffffffffff";
+let idealBlockTime = 2 * 1000; // In miliseconds
 let blockchain = [genesisBlock];
-let newBlock = new Block(1, [], genesisBlock.hash);
+let block_hash_length = 30; 
 
-console.log(newBlock);
+class Block {
+  constructor(index, transactions, prevHash, difficulty, timestamp, nonce) {
+    this.index = index;
+    this.transactions = transactions;
+    this.prevHash = prevHash;
+    this.difficulty = difficulty;
+    if (typeof timestamp === "undefined") {
+      this.timestamp = new Date().getTime();
+    }
 
-let difficulty = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+    if (typeof nonce === "undefined") {
+      this.nonce = 0;
+    } else {
+      this.nonce = nonce
+    }
 
-while (parseInt(newBlock.hash, 16) > parseInt(difficulty, 16)) {
- newBlock.updateNonce();
+    this.hash = crypto.createHash('sha256').update(this.toString()).digest('hex').substring(0, block_hash_length);
+  }
+
+  updateNonce() {
+    this.nonce += 1;
+    let updatedBlock = new Block(this.index, this.transactions, this.prevHash, this.difficulty, this.timestamp, this.nonce);
+    this.hash = updatedBlock.hash; 
+  }
+
+  toString() {
+    return JSON.stringify(this);
+  }
 }
 
-blockchain.push(newBlock);
+let hexAddition = (firstNumber, secondNumber) => {
+  return (parseInt(firstNumber, 16) + parseInt(secondNumber, 16)).toString(16);
+};
 
-console.log(blockchain);
+let hexSubtraction = (firstNumber, secondNumber) => {
+  return (parseInt(firstNumber, 16) - parseInt(secondNumber, 16)).toString(16);
+};
 
-console.log("Mined block => ", newBlock);
+let calculateDifficulty = (index) => {
+
+  if (blockchain.length < 3) {
+    return genesisBlock.difficulty;
+  }
+
+  let lastBlockDifficulty = blockchain[index - 1].difficulty;
+  let lastBlockTime = blockchain[index - 1].timestamp;
+  let blockBeforeLastBlockTime = blockchain[index - 2].timestamp;
+  let timeDifference = lastBlockTime - blockBeforeLastBlockTime;
+  console.log("difficulty " + lastBlockDifficulty);
+  console.log('Ideal Block Time', (idealBlockTime/1000), 'sec, Current Block Time', (timeDifference/1000), 'sec');
+
+  if (timeDifference > idealBlockTime) {
+    console.log('Higher Block Time, Decreaseing Difficulty');
+    return hexAddition(lastBlockDifficulty, difficultyStep);
+  }
+  else if (timeDifference < idealBlockTime) {
+    console.log('Higher Block Time, Increasing Difficulty');
+    return hexSubtraction(lastBlockDifficulty, difficultyStep);
+  }
+  else
+    return lastBlockDifficulty;
+};
+
+
+let createBlock = () => {
+  return new Promise((resolve) => {
+    console.log('Mining block', blockchain.length);
+    const new_difficulty = calculateDifficulty(blockchain.length);
+    let newBlock = new Block(blockchain.length, [], blockchain[blockchain.length - 1].hash, new_difficulty);
+
+    while (parseInt(newBlock.hash, 16) > parseInt(new_difficulty, 16)) {
+      newBlock.updateNonce();
+    }
+    resolve(newBlock);
+  });
+};
+
+
+function main() {
+  function loop() {
+    return createBlock()
+      .then(function (newBlock) {
+        blockchain.push(newBlock);
+        setTimeout(loop, 1000)
+      });
+  }
+
+  return loop();
+}
+
+main();
+
+
+
+
